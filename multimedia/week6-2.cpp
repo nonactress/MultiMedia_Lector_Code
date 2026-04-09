b88f63d9a58fa0a6ec08f1bae8fcc1f1064b1cbc
nonactress <tjgsuwls1007@gmail.com>
2026-04-09 16:21:53 +0900

    Update week6-2.cpp

--- multimedia/week6-2.cpp
+++ multimedia/week6-2.cpp
@@ -10,9 +10,13 @@ float getDist(CvScalar a, CvScalar b)
 
 	return sum;
 }
+
+/*
 int main()
 {
 	IplImage* src = cvLoadImage("c:\\temp\\lena.png");
+
+
 	CvSize size = cvGetSize(src);
 	IplImage* dst = cvCreateImage(size, 8, 3);
 	
@@ -23,6 +27,7 @@ int main()
 		cvScalar(255,255,255),cvScalar(192,192,192),
 	cvScalar(255,0,0),cvScalar(0,255,0),cvScalar(0,0,255)};
 	int num = 9;*/
+/*
 	CvScalar pal[] = {
 	cvScalar(0, 0, 0),       // Black
 	cvScalar(83, 37, 126),   // Dark Purple
@@ -54,6 +59,45 @@ int main()
 			}
 			
 			cvSet2D(dst, y, x,g);
+		}
+
+	cvShowImage("Src", src);
+	cvShowImage("dst", dst);
+	cvWaitKey();
+	return 0;
+
+}
+*/
+int main()
+{
+	IplImage* src = cvLoadImage("c:\\temp\\lena.png");
+
+
+	CvSize size = cvGetSize(src);
+	IplImage* dst = cvCreateImage(size, 8, 3);
+	IplImage* blur = cvCreateImage(size, 8, 3);
+	cvSet(dst, cvScalar(0, 0, 0));
+	int order[] = { 1,8,0,6,2,5,7,3,4};
+	int num = 10;
+
+	cvSmooth(src, blur, CV_BLUR, 5);
+
+	for(int y=0;y<size.height-2;y+=3)
+		for (int x = 0; x < size.width-2; x += 3) // 2Ä­¾¿ ¶Ù¹Ç·Î -1 ¹üÀ§ ÇØÁà¾ßÇÔ Áß¿ä 
+		{
+			int div = 255 / num;
+			CvScalar f = cvGet2D(blur, y, x);
+			int bri = (f.val[0] + f.val[1] + f.val[2]) / 3;
+
+			int n = bri / div;
+			if (n > 9)n = 9;
+			for (int i = 0; i < n; i++)
+			{
+				int u = order[i]%3;
+				int v = order[i]/3;
+
+				cvSet2D(dst, y+u, x+v, cvScalar(255, 255, 255));
+			}
 		}
 
 	cvShowImage("Src", src);
